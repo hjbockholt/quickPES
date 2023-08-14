e4254c1c70979f0288a5d4ae77d519754b66f68a
H. Jeremy Bockholt <hjbockholt@gmail.com>
2023-08-14 03:48:22 -0400

    Adjust UI Elements in ContentView
    - Reduce the width of the Subfolder Name text input field to match 20 characters.
    - Reorder UI elements for the "Select Target USB Directory" section.

--- QuickPES/ContentView.swift
+++ QuickPES/ContentView.swift
@@ -9,61 +9,87 @@ struct ContentView: View {
     
     var body: some View {
         VStack(spacing: 20) {
-            Text("QuickPES")
-                .font(.title)
-                .padding(.top)
-            
             Text("A utility tool to simplify the organization and transfer of .pes files for Brother embroidery systems.")
                 .font(.subheadline)
                 .foregroundColor(.gray)
                 .multilineTextAlignment(.center)
                 .padding(.horizontal)
             
-            Text("Drag & Drop your files or folders here:")
-                .onDrop(of: [UTType.fileURL.identifier], isTargeted: nil) { providers in
-                    _ = providers.first?.loadObject(ofClass: URL.self) { url, error in
-                        if let actualURL = url {
-                            self.selectedFiles = fetchPESFilesRecursively(from: actualURL)
+            HStack {
+                VStack {
+                    Text("Drag & Drop your files or folders here:")
+                        .onDrop(of: [UTType.fileURL.identifier], isTargeted: nil) { providers in
+                            _ = providers.first?.loadObject(ofClass: URL.self) { url, error in
+                                if let actualURL = url {
+                                    DispatchQueue.main.async {
+                                        self.selectedFiles = fetchPESFilesRecursively(from: actualURL)
+                                    }
+                                }
+                            }
+                            return true
                         }
+                        .frame(width: 300, height: 150)
+                        .background(Color.gray.opacity(0.2))
+                        .cornerRadius(8.0)
+                        .overlay(RoundedRectangle(cornerRadius: 8).stroke(Color.blue, lineWidth: 2))
+                    
+                    // Displaying the paths of the selected files
+                    if !selectedFiles.isEmpty {
+                        Text("Selected files:")
+                            .padding(.top, 10)
+                        
+                        ScrollView {
+                            ForEach(selectedFiles.indices, id: \.self) { index in
+                                Text(selectedFiles[index].path)
+                            }
+                        }
+                        .frame(height: 100)
                     }
-                    return true
                 }
-                .frame(width: 300, height: 150)
-                .background(Color.gray.opacity(0.2))
-                .cornerRadius(8.0)
-                .overlay(RoundedRectangle(cornerRadius: 8).stroke(Color.blue, lineWidth: 2))
+                
+                Spacer()
+                
+                VStack {
+                    Spacer()
+                    Button("Or Browse Files Manually") {
+                        showFilePicker()
+                    }
+                    Spacer()
+                }
+            }
             
-            // Displaying the paths of the selected files
-            if !selectedFiles.isEmpty {
-                Text("Selected files:")
-                    .padding(.top, 10)
-
-                ScrollView {
-                    ForEach(selectedFiles.indices, id: \.self) { index in
-                        Text(selectedFiles[index].path)
+            VStack(spacing: 20) {
+                HStack {
+                     Text("Subfolder Name:")
+                         .frame(width: 120, alignment: .trailing)
+                     TextField("Enter Subfolder Name", text: $subfolderName)
+                         .frame(width: 100) // Set the desired width here
+                         .padding(10)
+                         .overlay(
+                             RoundedRectangle(cornerRadius: 5)
+                                 .stroke(Color.gray, lineWidth: 1)
+                         )
+                         .onChange(of: subfolderName) { newName in
+                             if newName.count > 20 {
+                                 subfolderName = String(newName.prefix(20))
+                             }
+                         }
+                 }
+                HStack {
+                    Button("Select Target USB Directory") {
+                        showDirectoryPicker()
+                    }
+                    Spacer()
+                    Text("Selected USB:")
+                        .frame(width: 120, alignment: .trailing)
+                    if let usbURL = selectedUSB {
+                        Text(usbURL.path)
+                            .lineLimit(1)
+                            .truncationMode(.middle)
                     }
                 }
-                .frame(height: 100)  // Adjust the height accordingly
             }
-
-            Button("Or Browse Files Manually") {
-                showFilePicker()
-            }
-            
-            TextField("Subfolder Name", text: $subfolderName)
-                .padding(10)
-                .overlay(
-                    RoundedRectangle(cornerRadius: 5)
-                        .stroke(Color.gray, lineWidth: 1)
-                )
-
-            if let usbURL = selectedUSB {
-                Text("Selected USB: \(usbURL.path)")
-            }
-
-            Button("Select Target USB Directory") {
-                showDirectoryPicker()
-            }
+            .padding()
 
             Button("Copy .PES Files") {
                 if let usbURL = selectedUSB {
@@ -87,7 +113,7 @@ struct ContentView: View {
         panel.begin { result in
             if result == .OK {
                 let selectedURLs = panel.urls
-                self.selectedFiles = selectedURLs.flatMap { fetchPESFilesRecursively(from: $0) }
+                self.selectedFiles.append(contentsOf: selectedURLs)
             }
         }
     }
@@ -99,7 +125,7 @@ struct ContentView: View {
             for case let fileURL as URL in enumerator {
                 do {
                     let resourceValues = try fileURL.resourceValues(forKeys: [.isDirectoryKey])
-                    if let isDirectory = resourceValues.isDirectory, !isDirectory {  // Fixed here
+                    if let isDirectory = resourceValues.isDirectory, !isDirectory {
                         if fileURL.pathExtension == "pes" {
                             pesFiles.append(fileURL)
                         }
